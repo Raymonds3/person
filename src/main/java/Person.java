5c782c8f994f39d32e6d80e5b1a8f64c17f48688
Raymonds3 <raymonds338@gmail.com>
2020-02-10 14:12:24 +0200

    feedback update

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Person {
     String name;
     int age;
@@ -12,8 +14,23 @@ public class Person {
     }
 
     public String hello(){
-        
-        String message ="Hello, my name is " + name +" and I am "+ age + " years old. My interests "+ interest[0]+", " + interest[1]+" and " +interest[2]+".";
-        return message;
+        String message = "";
+        String punctuation="";
+        for(int i=0; i<interest.length; i++){
+            if(i>0 && i<interest.length-1){
+                punctuation += ", ";
+            }
+            if(i==interest.length-1){
+                punctuation += " and ";
+            }
+            punctuation += interest[i];
+        }
+//        for(int i=0; i<interest.length; i++){
+//
+//            message = "Hello, my name is " + name +" and I am "+ age + " years old. My interests are "+ interest[i] +".";
+//
+//        }
+        message = "Hello, my name is " + name +" and I am "+ age + " years old. My interests are ";
+        return message+punctuation+".";
     }
 }
